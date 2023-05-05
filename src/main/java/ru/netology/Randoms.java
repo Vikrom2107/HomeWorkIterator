1a0907df99c80498ac40b920898b8d2b988b8935
Viktor Romanov <vikrom2107@yandex.ru>
2023-05-05 10:20:05 +0700

    Changed method next()

--- src/main/java/ru/netology/Randoms.java
+++ src/main/java/ru/netology/Randoms.java
@@ -1,20 +1,15 @@
 package ru.netology;
 
 import java.util.Iterator;
-import java.util.List;
-import java.util.Objects;
 import java.util.Random;
-import java.util.function.Consumer;
-import java.util.stream.Collectors;
 
 public class Randoms implements Iterable<Integer> {
     protected Random random = new Random();
-    protected List<Integer> array;
+    protected int min;
+    protected int max;
     public Randoms(int min, int max) {
-
-        array = random.ints(100, min, max+1)
-                .boxed()
-                .collect(Collectors.toList());
+        this.min = min;
+        this.max = max;
     }
 
     @Override
@@ -22,7 +17,6 @@ public class Randoms implements Iterable<Integer> {
         return new RandomIterator<Integer>();
     }
     private class RandomIterator<Integer> implements Iterator{
-        int index = 0;
         @Override
         public boolean hasNext() {
             return true;
@@ -30,7 +24,9 @@ public class Randoms implements Iterable<Integer> {
 
         @Override
         public Object next() {
-            return array.get(index++);
+            return random.ints(100, min, max+1)
+                    .findAny()
+                    .getAsInt();
         }
     }
 }
